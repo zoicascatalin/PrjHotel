a4e5b3d736a5b517d95934c1eab190481ccf0091
Catalin Zoicas <smooke4ggyt@gmail.com>
2018-06-25 08:51:53 +0200

    commit 24/06/18

--- HotelHost/Home.aspx.cs
+++ HotelHost/Home.aspx.cs
@@ -11,7 +11,13 @@ namespace HotelBase
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            btnBooking1.Text = "Booking.com";
+            btnBooking2.Text = "Booking.com";
+            btnBooking3.Text = "Booking.com";
+        }
+        protected void btnBooking_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.booking.com/hotel/ae/aloft-abu-dhabi.en-gb.html?label=gen173nr-1DCAEoggJCAlhYSDNYBGhxiAEBmAEuwgEKd2luZG93cyAxMMgBDNgBA-gBAZICAXmoAgM;sid=9425a8f638bf05e336438ce36440eefc;dest_id=-782066;dest_type=city;dist=0;hapos=1;hpos=1;room1=A%2CA;sb_price_type=total;srepoch=1529492959;srfid=21e105c4f65a8c4ce023628696fac5828962b0f8X1;srpvid=60804e6f4b430348;type=total;ucfs=1&#hotelTmpl");
         }
     }
 }
---
+++ HotelHost/Login.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace HotelHost {
+    
+    
+    public partial class Login {
+        
+        /// <summary>
+        /// Controllo pnlBody.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlBody;
+        
+        /// <summary>
+        /// Controllo lblUsername.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsername;
+        
+        /// <summary>
+        /// Controllo txtUsername.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsername;
+        
+        /// <summary>
+        /// Controllo lblPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPassword;
+        
+        /// <summary>
+        /// Controllo txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        
+        /// <summary>
+        /// Controllo checkRememberUsername.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox checkRememberUsername;
+        
+        /// <summary>
+        /// Controllo checkStayLogged.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox checkStayLogged;
+        
+        /// <summary>
+        /// Controllo btnConfirm.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnConfirm;
+        
+        /// <summary>
+        /// Controllo lblWrongData.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblWrongData;
+    }
+}
